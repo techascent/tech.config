a0ce088684f39cf89dd0c1726ee65508cb2595cd
Chris Nuernberger <chris@thinktopic.com>
2018-06-05 12:44:53 -0600

    Release 0.3.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.config "0.3.5-SNAPSHOT"
+(defproject techascent/tech.config "0.3.5"
   :description "A configuartion library."
   :url "http://github.com/thinktopic/think.config"
 
