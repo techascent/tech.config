f08af0910ded1ca0d896c3756ada6cc4978d19e5
Chris Nuernberger <chris@thinktopic.com>
2019-01-01 15:20:34 -0700

    0.3.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.config "0.3.6-SNAPSHOT"
+(defproject techascent/tech.config "0.3.6"
   :description "A configuration library."
   :url "http://github.com/techascent/tech.config"
 
