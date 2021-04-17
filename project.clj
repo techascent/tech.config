ef0cdc80bbb5ba58741888c78ec1db5b4dc93410
Chris Nuernberger <chris@techascent.com>
2021-04-17 06:08:22 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.config "0.3.10"
+(defproject techascent/tech.config "0.3.11-SNAPSHOT"
   :description "A configuration library."
   :url "http://github.com/techascent/tech.config"
 
